efeed8797ebcc7bf09a48628f94f3ce4d1e92e3f
shenshuo <shenshuo@shinezone.com>
2019-05-24 10:31:47 +0800

    超级用户直接拥有所有菜单权限

--- mg/handlers/login_handler.py
+++ mg/handlers/login_handler.py
@@ -141,7 +141,7 @@ class LoginHandler(RequestHandler):
         my_verify = MyVerify(user_id, is_superuser)
         my_verify.write_verify()
         ### 前端权限写入缓存
-        get_user_rules(user_id)
+        get_user_rules(user_id, is_superuser)
 
         return self.write(dict(code=0, auth_key=auth_key.decode(), username=user_info.username,
                                nickname=user_info.nickname, msg='登录成功'))
@@ -205,15 +205,9 @@ class AuthorizationHandler(BaseHandler):
 #         print(data)
 #         return self.write(dict(data=data, code=0, msg='获取前端权限成功'))
 
-def get_user_rules(user_id,is_superuser=False):
+def get_user_rules(user_id, is_superuser=False):
     page_data = {}
     component_data = {}
-
-    if is_superuser:
-        page_data['all'] = True
-        component_data['all'] = True
-        redis_conn = cache_conn()
-        redis_conn.hmset("{}_rules".format(user_id), dict(page=page_data, component=component_data))
 
     with DBContext('r') as session:
         this_menus = session.query(Menus.menu_name
@@ -231,8 +225,12 @@ def get_user_rules(user_id,is_superuser=False):
         component_data[c[0]] = True
 
     ## 插入一个没有权限的
-    page_data['all'] = False
-    component_data['all'] = False
+    if is_superuser:
+        page_data['all'] = True
+        component_data['all'] = True
+    else:
+        page_data['all'] = False
+        component_data['all'] = False
     redis_conn = cache_conn()
     redis_conn.hmset("{}_rules".format(user_id), dict(page=page_data, component=component_data))
 
