111dfc03d525bc7cf879cf4ceab4c84aabcd5c54
shenshuo <191715030@qq.com>
2019-07-26 16:05:55 +0800

    修改用户树的id

--- mg/handlers/users_handler.py
+++ mg/handlers/users_handler.py
@@ -53,6 +53,7 @@ class UserHandler(BaseHandler):
             user_list.append(data_dict)
 
         redis_conn = cache_conn()
+        redis_conn.delete(const.USERS_INFO) ### 清空集合数据
         with redis_conn.pipeline(transaction=False) as p:
             for msg in all_user:
                 data_dict = model_to_dict(msg)
@@ -184,24 +185,19 @@ class UserTreeHandler(BaseHandler):
             data_dict = model_to_dict(msg)
             user_list.append(data_dict)
 
-        _tree = [{"expand": True, "disableCheckbox": True, "title": "all", "children": []}]
-
+        _tree = [{"label": "all", "children": []}]
         if user_list:
-            tmp_tree = {
-                "department": {},
-                "nickname": {},
-            }
+            tmp_tree = {"department": {}, "nickname": {}}
 
             for t in user_list:
-                department, nickname = t["department"], t['nickname']
+                department, nickname, user_id = t["department"], t['nickname'], t['user_id']
 
                 # 因为是第一层所以没有parent
-                tmp_tree["department"][department] = {
-                    "expand": False, "disableCheckbox": True, "title": department, "parent": "all", "children": [],
-                }
+                tmp_tree["department"][department] = {"label": department, "parent": "all", "children": [],
+                                                      "id": shortuuid.uuid()}
 
                 tmp_tree["nickname"][department + "|" + nickname] = {
-                    "title": nickname, "parent": department, "department": department
+                    "label": nickname, "parent": department, "department": department, "id": nickname
                 }
 
             for tmp_group in tmp_tree["nickname"].values():
@@ -210,9 +206,9 @@ class UserTreeHandler(BaseHandler):
             for tmp_git in tmp_tree["department"].values():
                 _tree[0]["children"].append(tmp_git)
 
-            return self.write(dict(code=0, msg='获取用户Tree成功', data=_tree))
+            return self.write(dict(code=0, msg='获取用户Tree成功', data=_tree[0]["children"]))
         else:
-            return self.write(dict(code=0, msg='获取用户Tree失败', data=_tree))
+            return self.write(dict(code=0, msg='获取用户Tree失败', data=_tree[0]["children"]))
 
 
 user_mg_urls = [
