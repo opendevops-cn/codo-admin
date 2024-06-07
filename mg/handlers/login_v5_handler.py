b6ac0837fbb2be422e677d2ab106c68380d4ced2
shenshuo <191715030@qq.com>
2024-06-07 09:54:03 +0800

    2024年6月07日  header日志01

--- mg/handlers/login_v5_handler.py
+++ mg/handlers/login_v5_handler.py
@@ -138,9 +138,9 @@ class LogoutHandler(RequestHandler, ABC):
 
     def get(self):
         try:
-            root_domain = self.request.headers.get('_codo_root_domain')
+            root_domain = self.request.headers.get('Codo-root-domain')
             print(self.request.headers)
-            logging.error(f"_codo_root_domain {root_domain}")
+            logging.error(f"Codo-root-domain {root_domain}")
             self.clear_all_cookies()
             self.clear_cookie("auth_key", domain=root_domain)
             self.clear_cookie("is_login", domain=root_domain)
