e14c82ae4807db85bbb7121a983f014241586834
shenshuo <191715030@qq.com>
2024-12-16 11:35:26 +0800

    2024年12月16日  优化初始化数据

--- docs/sql/codo_a_user_role.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `codo_a_user_role` (`id`, `role_id`, `user_id`) VALUES (120, 2, 1);
-INSERT INTO `codo_a_user_role` (`id`, `role_id`, `user_id`) VALUES (122, 2, 1054);
-INSERT INTO `codo_a_user_role` (`id`, `role_id`, `user_id`) VALUES (121, 2, 1700);
-INSERT INTO `codo_a_user_role` (`id`, `role_id`, `user_id`) VALUES (117, 19, 1256);
-INSERT INTO `codo_a_user_role` (`id`, `role_id`, `user_id`) VALUES (118, 19, 1257);
-INSERT INTO `codo_a_user_role` (`id`, `role_id`, `user_id`) VALUES (119, 19, 1258);
-INSERT INTO `codo_a_user_role` (`id`, `role_id`, `user_id`) VALUES (114, 21, 1311);
-INSERT INTO `codo_a_user_role` (`id`, `role_id`, `user_id`) VALUES (111, 24, 1054);
-INSERT INTO `codo_a_user_role` (`id`, `role_id`, `user_id`) VALUES (113, 24, 1311);
