f254d7fad460e1598018dbc620c14fe5726dafbf
shenshuo <191715030@qq.com>
2025-01-08 16:16:34 +0800

    2025年01月08日   完善用户手册

--- startup.py
+++ startup.py
@@ -11,7 +11,6 @@ from tornado.options import define
 from websdk2.program import MainProgram
 from settings import settings as app_settings
 from mg.applications import Application as MgApp
-from mg.subscribe import RedisSubscriber as SubApp
 from libs.registration import Registration
 
 define("service", default='api', help="start service flag", type=str)
@@ -23,8 +22,6 @@ class MyProgram(MainProgram):
         settings = app_settings
         if service in ['admin-mg-api', 'mg-api', 'mg', 'api', 'adminv4'] or not service:
             self.__app = MgApp(**settings)
-        elif service in ['sub_log', 'gw_log', 'gw-log', 'admin-gw-log']:
-            self.__app = SubApp(service=service, **settings)
         elif service == 'init':
             self.__app = Registration(**settings)
         super(MyProgram, self).__init__(progressid)
@@ -38,5 +35,5 @@ if __name__ == '__main__':
 python3 startup.py --service=mg --port=8010
 python3 startup.py --service=init
 
-docker build  --no-cache --build-arg SERVICE_NAME=mg  . -t codo-admin-image
+docker build  --no-cache --build-arg SERVICE_NAME=api  . -t codo-admin-image
 """
