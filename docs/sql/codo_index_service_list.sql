83c41a5cf467e175a9d7d3b35d144a52c68d14c3
shenshuo <191715030@qq.com>
2025-03-13 19:26:03 +0800

    2025年3月13日  优化数据

--- docs/sql/codo_index_service_list.sql
+++ docs/sql/codo_index_service_list.sql
@@ -1,8 +1,1 @@
--- INSERT INTO `codo_index_service_list` (`create_time`, `update_time`, `id`, `name`, `img`, `description`) VALUES ('2024-12-10 09:30:25', '2024-12-10 12:06:10', 2, 'ROO', 'ROO', 'ROO');
--- INSERT INTO `codo_index_service_list` (`create_time`, `update_time`, `id`, `name`, `img`, `description`) VALUES ('2024-12-10 11:08:28', '2024-12-10 11:08:28', 3, 'RO3', 'RO3', 'RO3');
--- INSERT INTO `codo_index_service_list` (`create_time`, `update_time`, `id`, `name`, `img`, `description`) VALUES ('2024-12-10 11:08:37', '2024-12-10 11:08:37', 4, '美食2', '美食2', '美食2');
--- INSERT INTO `codo_index_service_list` (`create_time`, `update_time`, `id`, `name`, `img`, `description`) VALUES ('2024-12-10 11:08:48', '2024-12-10 11:08:48', 5, 'CBB', 'CBB', 'CBB');
-INSERT INTO `codo_index_service_list` (`create_time`, `update_time`, `id`, `name`, `img`, `description`) VALUES ('2024-12-10 11:08:57', '2024-12-10 11:08:57', 6, '平台', '平台', '平台');
--- INSERT INTO `codo_index_service_list` (`create_time`, `update_time`, `id`, `name`, `img`, `description`) VALUES ('2024-12-10 11:09:24', '2024-12-10 11:09:24', 7, 'ACT', 'ACT', 'ACT');
--- INSERT INTO `codo_index_service_list` (`create_time`, `update_time`, `id`, `name`, `img`, `description`) VALUES ('2024-12-10 18:05:29', '2024-12-10 19:01:16', 14, 'ROW', 'ROW', 'ROW');
-INSERT INTO `codo_index_service_list` (`create_time`, `update_time`, `id`, `name`, `img`, `description`) VALUES ('2024-12-10 18:55:51', '2024-12-10 18:55:51', 15, 'TEST', 'test', 'test');
+
